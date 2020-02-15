46e29db3027ce1c00e4ca86c39c02d0f50288bf2
Daniel Muyshond <daniel.muyshond@gmail.com>
2020-02-15 11:34:25 +0100

    Open urls lists

---
+++ OpenUrlList/01-open_urls.py
@@ -0,0 +1,12 @@
+import webbrowser
+
+urls = ['https://www.gmail.com', 'https://www.inoreader.com', 'https://www.meteobelgique.com', 'https://github.com/dmshd/Python', 'https://docs.python.org/3.7/library/webbrowser.html?highlight=webbrowser']
+counter = 0
+
+for website in urls:
+    webbrowser.open_new_tab(website)
+    print("Loading " + website + "...")
+    counter += 1
+
+print(str(counter) + " websites loaded !")
+  
