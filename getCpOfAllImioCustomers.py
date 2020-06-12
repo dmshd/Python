2d7ff3f769ef6ad3f116a6039d3c16da944a17c9
Daniel Muyshond <daniel.muyshond@gmail.com>
2020-06-12 21:43:47 +0200

    iMio postal code project init

---
+++ getCpOfAllImioCustomers.py
@@ -0,0 +1,7 @@
+# The general idea is to make an easy and faster way to get a postal code of one of our client
+# 1. Query http://geoservices.wallonie.be/geolocalisation/rest/searchLocalites/namur   (namur for example) 
+#    for each of client I can get through infra-api 
+# 2. Write these data in a JSON object
+# 3. Make an API that return just what I need
+
+
