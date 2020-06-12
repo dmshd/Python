b402faaf03dbdae12a2bacc85a7ab42a56091a2a
Daniel Muyshond <daniel.muyshond@gmail.com>
2020-06-12 21:41:41 +0200

    adds urls

--- OpenUrlList/01-open_urls.py
+++ OpenUrlList/01-open_urls.py
@@ -1,6 +1,14 @@
 import webbrowser
 
-urls = ['https://www.gmail.com', 'https://www.inoreader.com', 'https://www.meteobelgique.com', 'https://github.com/dmshd/Python', 'https://docs.python.org/3.7/library/webbrowser.html?highlight=webbrowser']
+urls = [
+        'https://www.gmail.com',
+        'https://www.inoreader.com',
+        'https://www.developpez.com/',
+        'https://www.meteobelgique.com',
+        'https://github.com/dmshd/Python',
+        'https://docs.python.org/3.7/library/webbrowser.html?highlight=webbrowser'
+        ]
+
 counter = 0
 
 for website in urls:
