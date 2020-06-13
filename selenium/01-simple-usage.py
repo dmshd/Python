b815b6faca8853ddd1b20f1688db933b1ea0f726
Daniel Muyshond <daniel.muyshond@gmail.com>
2020-06-13 15:14:46 +0200

    init selenium foldr

---
+++ selenium/01-simple-usage.py
@@ -0,0 +1,22 @@
+# I'm reading the official doc
+# https://selenium-python.readthedocs.io/getting-started.html
+
+# webdriver module provides all the WebDriver implementations.
+from selenium import webdriver 
+from selenium.webdriver.common.keys import Keys
+
+# create instance of Firefox WebDriver
+driver = webdriver.Firefox()
+
+# get() is used to navigate
+driver.get("http://www.python.org")
+
+# Assertion to confirm that title has "Python" word in it
+assert "Python" in driver.title
+elem = driver.find_element_by_name("q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+driver.close()
+
