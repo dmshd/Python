034437065993a30a98b90603d03bc61b0e6f8525
Daniel Muyshond <daniel.muyshond@gmail.com>
2020-06-13 16:55:56 +0200

    second doc example

---
+++ selenium/02-write-tests.py
@@ -0,0 +1,34 @@
+# a test for python.org search functionnality
+
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+# 
+
+class PythonOrgSearch(unittest.TestCase):
+
+  # initialization, seUp will get called before every test
+  # function written with this test class.
+
+  def setUp(self):
+    self.driver = webdriver.Firefox()
+
+  # test case method should always start with "test"
+  # the first line inside this method create a local
+  # reerence to the driver object created in setUp()
+  
+  def test_search_in_python_org(self):
+    driver = self.driver
+    driver.get("http://www.python.org")
+    self.assertIn("Python", driver.title)
+    elem = driver.find_element_by_name("q")
+    elem.send_keys("pycon")
+    elem.send_keys(Keys.RETURN)
+    assert "No results found." not in driver.page_source
+
+  def tearDown(self):
+    self.driver.close()
+
+if __name__ == "__main__":
+  unittest.main()
