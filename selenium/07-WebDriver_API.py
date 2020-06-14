4fe176951c07390bc34f8df19516c4708c99f0dc
Daniel Muyshond <daniel.muyshond@gmail.com>
2020-06-14 20:01:52 +0200

    webdriver api init

---
+++ selenium/07-WebDriver_API.py
@@ -0,0 +1,19 @@
+# WebDriver API
+# https://selenium-python.readthedocs.io/api.html
+
+
+from selenium import webdriver
+
+webdriver.Firefox
+webdriver.FirefoxProfile
+webdriver.Chrome
+webdriver.ChromeOptions
+webdriver.Ie
+webdriver.Opera
+webdriver.PhantomJS
+webdriver.Remote
+webdriver.DesiredCapabilities
+webdriver.ActionChains
+webdriver.TouchActions
+webdriver.Proxy
+
