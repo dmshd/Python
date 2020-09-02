c86f5974d8377094706f5d2e014277620db9db44
Daniel Muyshond <daniel.muyshond@gmail.com>
2020-09-02 21:22:37 +0200

    add range.py (python start here tut)

---
+++ range.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+a = ['et', 'mon', 'cul', 'c\'est', 'du', 'poulet', '?']
+
+for i in range(len(a)):
+    print(i, a[i])
+    
+for i in range(len(a)):
+    print(a[i])
+
+# In most such cases, however, it is convenient to use the
+# enumerate() function, see Looping Techniques.
