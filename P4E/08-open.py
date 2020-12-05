b2c0090fbcbb96a019b56a0de7ab307a2c5b0265
Daniel <imac2020@iMac.local>
2020-12-05 21:48:36 +0100

    add next ex

---
+++ P4E/08-open.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+"""
+7.1 Write a program that prompts for a file name, then opens that file and reads
+through the file, and print the contents of the file in upper case. Use the file
+words.txt to produce the output below.
+You can download the sample data at http://www.py4e.com/code3/words.txt
+"""
+
+# Use words.txt as the file name
+fname = input("Enter file name: ")
+try:
+	fhand = open(fname)
+except:
+    print('File not found:', fhand)
+    exit()
+
+for line in fhand:
+    line = line.rstrip()
+    line = line.upper()
+    print(line)
